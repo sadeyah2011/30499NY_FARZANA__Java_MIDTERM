ebdeac6bc6ba4ac81012406cd2d5e5aff7aea677
sadeyah2011 <fakter6803@gmail.com>
2022-10-14 00:43:51 -0400

    short answar4

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -2,12 +2,30 @@ package math_problems;
 
 public class FindMissingNumber {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a method to find the missing number from the array.
      */
 
     public static void main(String[] args) {
-        int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
+        int[] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+    }
+
+    static void printMissingElements(int arr[], int
+                                     int diff = arr[0] - 0;
+    for(
+    int i = 0;
+    i<N;i++)
+            if(arr[i]-i !=diff)
+            while(diff<arr[i]-i)
+
+    {
+        System.out.print((i + diff) + " ");
+        diff++;
+    }
+}
+}
+
 
     }
 
--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -15,3 +15,26 @@ public class PrimeNumber {
     }
 
 }
+public static void main(String[] args) {
+
+    printPrimeNumbers(2, 1_000_000);
+}
+public static void printPrimeNumbers (int start, int end) {
+    int counter;
+    for (int i = start; i <= end; i++) {
+        counter = 0;
+        for (int j = start + 1; j <= i / 2; j++) {
+            if (i % j == 0) {
+                counter++;
+                break;
+            }
+        }
+        if (counter == 0) {
+            System.out.println(i);
+        }
+    }
+}
+
+        }
+    }
+}
